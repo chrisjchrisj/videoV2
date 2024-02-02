854fd99746151651a8c3db8713391432bcf2b67c
chrisjchrisj <chrisjames123@hotmail.com>
2024-02-02 09:10:46 -0800

    added Flutter Auth-X lines/dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:video/camera_page.dart';
-//import 'package:firebase_auth/firebase_auth.dart';
-
-
-//future main() async {
-  //WidgetsFlutterBinding.ensureInitialized();
- // await Firebase.initializeApp();
- // runApp(MyApp());
-//}
+import 'package:get/get.dart';
+import 'package:to_do_app/res/routes/app_routes.dart';
+import 'package:to_do_app/view/sign%20up/sign_up.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -35,6 +30,20 @@ class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   _MyAppState createState() => _MyAppState();
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      getPages: AppRoutes.routes(),
+    );
 }
 
 class _MyAppState extends State<MyApp> {
