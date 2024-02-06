4a2e6e362f04237b3dedd6598c99a7e21bc3cd99
chrisjchrisj <chrisjames123@hotmail.com>
2024-02-06 10:20:31 -0800

    replace

--- lib/res/routes/app_routes.dart
+++ lib/res/routes/app_routes.dart
@@ -1,9 +1,9 @@
 import 'package:get/get.dart';
-import 'package:to_do_app/res/routes/routes.dart';
-import 'package:to_do_app/view/home%20page/home_page.dart';
-import 'package:to_do_app/view/sign%20in/sign_in.dart';
-import 'package:to_do_app/view/sign%20up/sign_up.dart';
-import 'package:to_do_app/view/splash/splash_screen.dart';
+import 'package:video/res/routes/routes.dart';
+import 'package:video/view/home%20page/home_page.dart';
+import 'package:video/view/sign%20in/sign_in.dart';
+import 'package:video/view/sign%20up/sign_up.dart';
+import 'package:video/view/splash/splash_screen.dart';
 
 class AppRoutes{
   static List<GetPage> routes(){
