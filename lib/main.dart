0d1968caa91fed46d922e7fcff065d0e7676d164
chrisjchrisj <chrisjames123@hotmail.com>
2024-02-07 10:33:24 -0800

    no camera dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
-import 'package:camera/camera.dart';
+//import 'package:camera/camera.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-//import 'package:video/camera_page.dart';
+import 'package:video/camera_page.dart';
 import 'package:video/video_screen.dart';
 
 void main() async {
