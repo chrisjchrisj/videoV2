5644cdf0840687528b9d5a32079031a4749b0131
chrisjchrisj <chrisjames123@hotmail.com>
2024-02-07 10:12:08 -0800

    video_screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,137 +3,50 @@ import 'package:camera/camera.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:video/res/routes/app_routes.dart';
-import 'package:video/view/sign%20up/sign_up.dart';
-
-late List<CameraDescription> cameras;
+//import 'package:video/camera_page.dart';
+import 'package:video/video_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  cameras = await availableCameras();
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      getPages: AppRoutes.routes(),
-      home: App(),
-    );
-  }
+  camera = await availableCameras();
+  Firebase.initializeApp();
+  runApp(App());
 }
 
 class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
+  const App({
+    Key? key,
+  }) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyApp());
+  }
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: const Center(
         child: Text(
-          'Flutter Camera and Video Recording app',
+          'Flutter camera and video recording app',
           style: TextStyle(fontSize: 20),
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
-          Navigator.push(
-              context,
-              CupertinoPageRoute(
-                  builder: (context) => CameraScreen(cameras: cameras)));
+          Navigator.push(context,
+              CupertinoPageRoute(builder: (context) => cameraScreen()));
         },
         label: const Text('start'),
       ),
     );
   }
 }
-
-class CameraScreen extends StatefulWidget {
-  final List<CameraDescription> cameras;
-
-  const CameraScreen({Key? key, required this.cameras}) : super(key: key);
-
-  @override
-  _CameraScreenState createState() => _CameraScreenState();
-}
-
-class _CameraScreenState extends State<CameraScreen> {
-  late CameraController _controller;
-  late Future<void> _initializeControllerFuture;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = CameraController(
-      widget.cameras.first,
-      ResolutionPreset.medium,
-    );
-    _initializeControllerFuture = _controller.initialize();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Camera Preview'),
-      ),
-      body: FutureBuilder<void>(
-        future: _initializeControllerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return CameraPreview(_controller);
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          try {
-            await _initializeControllerFuture;
-            final image = await _controller.takePicture();
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => DisplayPictureScreen(imagePath: image.path),
-              ),
-            );
-          } catch (e) {
-            print(e);
-          }
-        },
-        child: const Icon(Icons.camera_alt),
-      ),
-    );
-  }
-}
-
-//class DisplayPictureScreen extends StatelessWidget {
- // final String imagePath;
-
-  //const DisplayPictureScreen({Key? key, required this.imagePath}) : super(key: key);
-
- // @override
- // Widget build(BuildContext context) {
-   // return Scaffold(
-    //  appBar: AppBar(title: const Text('Display the Picture')),
-    //  body: Image.file(File(imagePath)),
-  //  );
- // }
-//}
