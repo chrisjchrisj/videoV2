60cf2bc9a03aaa100e3e193f62c390966bf6b79e
chris <chrisjames123@hotmail.com>
2024-02-09 14:06:17 -0800

    camera

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-//import 'package:camera/camera.dart';
+import 'package:camera/camera.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
