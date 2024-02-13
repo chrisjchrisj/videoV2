9187f5728f674e600bfd89b2401a59558a83374b
chris <chrisjames123@hotmail.com>
2024-02-13 11:01:22 -0800

    update login

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:video/camera_page.dart'; // Import the video screen file
+import 'package:video/camera_page.dart'; // Import the camera page file
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -19,10 +19,11 @@ class _LoginScreenState extends State<LoginScreen> {
         email: _emailController.text.trim(),
         password: _passwordController.text.trim(),
       );
+
       // Navigate to the camera page after successful login
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => CameraPage()), // Navigate to CameraPage
+        MaterialPageRoute(builder: (context) => cameraScreen()), // Navigate to cameraScreen
       );
     } catch (e) {
       // Handle sign-in errors here
