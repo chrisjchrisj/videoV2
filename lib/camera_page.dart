aaf4f9a78b91f0dbcaeead091d5da47cb5c8ecba
chris <chrisjames123@hotmail.com>
2024-02-14 10:48:26 -0800

    replace with current

--- lib/camera_page.dart
+++ lib/camera_page.dart
@@ -1,85 +1,73 @@
 //to record video
 import 'dart:async';
+import 'dart:developer';
 import 'dart:io';
 import 'dart:typed_data';
+
 import 'package:camera/camera.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:uuid/uuid.dart';
-import 'package:video_player/video_player.dart';
 
-class sendVideo extends StatefulWidget {
-  XFile imagepath;
-  sendVideo({Key? key, required this.imagepath}) : super(key: key);
+List<CameraDescription>? camera;
+
+class CameraScreen extends StatefulWidget {
+  CameraScreen({Key? key}) : super(key: key);
 
   @override
-  State<sendVideo> createState() => _sendVideoState();
+  State<CameraScreen> createState() => _CameraScreenState();
 }
 
-class _sendVideoState extends State<sendVideo> {
-  VideoPlayerController? _controller;
-  bool _isUploading = false;
+class _CameraScreenState extends State<CameraScreen> {
+  CameraController? _cameraController;
+  late Future<void> cameraValue;
   double _uploadProgress = 0;
-  late Reference _storageReference;
 
   @override
   void initState() {
+    _cameraController = CameraController(camera![0], ResolutionPreset.high);
+    cameraValue = _cameraController!.initialize();
     super.initState();
-    _controller = VideoPlayerController.file(File(widget.imagepath.path))
-      ..initialize().then((_) {
-        setState(() {});
-      });
-    _storageReference = FirebaseStorage.instance.ref().child('videos');    
   }
+
+  bool flip = true;
+  bool flash = false;
+  XFile? photo;
 
   @override
   void dispose() {
-    _controller!.dispose();
+    _cameraController!.dispose();
     super.dispose();
   }
 
+  bool isrecording = false;
+  int zoom = 1;
+  bool show = false;
+
   Future<void> _stopRecordingAndUpload() async {
     // Stop the video recording
-    await _controller!.pause();
+    XFile? videoFile = await _cameraController!.stopVideoRecording();
 
-    setState(() {
-      _isUploading = true;
-    });
-
-    try {
-      await _sendVideoStream();
-      // Show upload success message
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Upload succeeded'),
-          backgroundColor: Colors.green,
-        ),
-      );
-    } catch (e) {
-      print(e);
-      // Show upload failure message
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Upload failed'),
-          backgroundColor: Colors.red,
-        ),
-      );
-    } finally {
-      setState(() {
-        _isUploading = false;
-      });
+    // Upload the recorded video if a file was successfully created
+    if (videoFile != null) {
+      await _sendVideoStream(videoFile);
+    } else {
+      print('Failed to stop video recording');
     }
   }
 
-  Future<void> _sendVideoStream() async {
+  Future<void> _sendVideoStream(XFile videoFile) async {
     // Get the video file as bytes
-    Uint8List bytes = await File(widget.imagepath.path).readAsBytes();
-  
+    Uint8List bytes = await File(videoFile.path).readAsBytes();
+
     // Upload the video bytes to Firebase Storage
     var postId = Uuid().v1();
-    UploadTask task = _storageReference.child('$postId.mp4').putData(bytes);
-  
+    Reference storageReference = FirebaseStorage.instance.ref().child('videos');
+    UploadTask task = storageReference.child('$postId.mp4').putData(bytes);
+
     // Track the upload progress
     task.snapshotEvents.listen((TaskSnapshot snapshot) {
       double progress = snapshot.bytesTransferred / snapshot.totalBytes;
@@ -87,13 +75,13 @@ class _sendVideoState extends State<sendVideo> {
         _uploadProgress = progress;
       });
     });
-  
+
     // Wait for the upload to complete
     await task;
-  
+
     // Get the download URL of the uploaded video
-    String downloadUrl = await _storageReference.child('$postId.mp4').getDownloadURL();
-  
+    String downloadUrl = await storageReference.child('$postId.mp4').getDownloadURL();
+
     // Print the download URL
     print('Download URL: $downloadUrl');
   }
@@ -101,94 +89,257 @@ class _sendVideoState extends State<sendVideo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: _isUploading
-          ? CircularProgressIndicator()
-          : FloatingActionButton(
-              onPressed: _stopRecordingAndUpload,
-              child: Icon(Icons.stop),
-            ),
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        leading: CupertinoButton(
-          child: Icon(
-            Icons.arrow_back_ios,
-            color: Theme.of(context).bottomNavigationBarTheme.backgroundColor == Colors.white
-                ? Colors.black
-                : Colors.white,
-          ),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        backgroundColor: Theme.of(context).bottomNavigationBarTheme.backgroundColor,
-        actions: [
-          Row(
-            children: [
-              IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.crop,
-                ),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.emoji_emotions,
-                ),
-              ),
-            ],
-          )
-        ],
-      ),
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.transparent,
+      extendBodyBehindAppBar: true,
       body: SafeArea(
         child: Stack(
-          alignment: Alignment.bottomCenter,
           children: [
-            Container(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              child: AspectRatio(
-                aspectRatio: _controller!.value.aspectRatio,
-                child: VideoPlayer(_controller!),
-              ),
+            FutureBuilder(
+              future: cameraValue,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return GestureDetector(
+                  onTap: () async {
+                    await _cameraController!.setFocusMode(FocusMode.auto);
+                  },
+                  onLongPress: () async {
+                    await _cameraController!.setFocusMode(FocusMode.locked);
+                  },
+                  child: CameraPreview(_cameraController!),
+                );
+              },
             ),
-            if (_isUploading)
-              Positioned.fill(
-                child: Center(
-                  child: CircularProgressIndicator(
-                    value: _uploadProgress,
-                    strokeWidth: 10,
-                  ),
-                ),
-              ),
-            SizedBox(
-              height: 20,
-            ),
-            Center(
-              child: InkWell(
-                onTap: () {
-                  if (_controller!.value.isPlaying) {
-                    setState(() {
-                      _controller!.pause();
-                    });
-                  } else {
-                    setState(() {
-                      _controller!.play();
-                    });
-                  }
-                },
-                child: CircleAvatar(
-                  backgroundColor: Colors.black.withOpacity(0.4),
-                  radius: 24,
-                  child: Center(
-                    child: Icon(
-                      _controller!.value.isPlaying ? Icons.pause : Icons.play_arrow,
-                      size: 28,
+            Align(
+              alignment: Alignment.topCenter,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(
+                      Icons.close,
                       color: Colors.white,
                     ),
                   ),
-                ),
+                  IconButton(
+                    onPressed: () {
+                      if (!flip) {
+                        flash = false;
+                      } else {
+                        if (flash) {
+                          setState(() {
+                            flash = false;
+                          });
+                        } else {
+                          setState(() {
+                            flash = true;
+                          });
+                        }
+                      }
+                      flash
+                          ? _cameraController!.setFlashMode(FlashMode.torch)
+                          : _cameraController!.setFlashMode(FlashMode.off);
+                    },
+                    icon: Icon(
+                      flash ? Icons.flash_on : Icons.flash_off,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 10,
+              right: 0,
+              left: 0,
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Column(
+                        children: [
+                          SizedBox(
+                            height: 25,
+                          ),
+                          !flip
+                              ? Container(
+                                  height: 27,
+                                  width: 40,
+                                  color: Colors.transparent,
+                                )
+                              : zoom == 10
+                                  ? GestureDetector(
+                                      onTap: () async {
+                                        setState(() {
+                                          zoom = 1;
+                                          show = false;
+                                        });
+                                        await _cameraController!
+                                            .setZoomLevel(zoom.toDouble());
+                                      },
+                                      child: CircleAvatar(
+                                        radius: 20,
+                                        backgroundColor: Colors.white30,
+                                        child: Text(
+                                          '10x',
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                      ),
+                                    )
+                                  : zoom == 4
+                                      ? GestureDetector(
+                                          onTap: () async {
+                                            setState(() {
+                                              zoom = 10;
+                                            });
+                                            await _cameraController!
+                                                .setZoomLevel(
+                                                    zoom.toDouble());
+                                          },
+                                          child: CircleAvatar(
+                                            radius: 20,
+                                            backgroundColor: Colors.white30,
+                                            child: Text(
+                                              '4x',
+                                              style: TextStyle(
+                                                  color: Colors.white),
+                                            ),
+                                          ),
+                                        )
+                                      : zoom == 2
+                                          ? GestureDetector(
+                                              onTap: () async {
+                                                setState(() {
+                                                  zoom = 4;
+                                                });
+                                                await _cameraController!
+                                                    .setZoomLevel(
+                                                        zoom.toDouble());
+                                              },
+                                              child: CircleAvatar(
+                                                radius: 20,
+                                                backgroundColor: Colors.white30,
+                                                child: Text(
+                                                  '2x',
+                                                  style: TextStyle(
+                                                      color: Colors.white),
+                                                ),
+                                              ),
+                                            )
+                                          : GestureDetector(
+                                              onTap: () async {
+                                                setState(() {
+                                                  zoom = 2;
+                                                  show = true;
+                                                });
+                                                await _cameraController!
+                                                    .setZoomLevel(
+                                                        zoom.toDouble());
+                                              },
+                                              child: CircleAvatar(
+                                                radius: 20,
+                                                backgroundColor: Colors.white30,
+                                                child: Text(
+                                                  '${zoom}x',
+                                                  style: TextStyle(
+                                                      color: Colors.white),
+                                                ),
+                                              ),
+                                            ),
+                          !flip
+                              ? Container(
+                                  height: 27,
+                                  width: 40,
+                                  color: Colors.transparent,
+                                )
+                              : GestureDetector(
+                                  onTap: () {
+                                    if (show) {
+                                      setState(() {
+                                        show = false;
+                                      });
+                                    } else {
+                                      setState(() {
+                                        show = true;
+                                      });
+                                    }
+                                  },
+                                  child: Icon(
+                                    show
+                                        ? Icons.arrow_drop_up
+                                        : Icons.arrow_drop_down,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                        ],
+                      ),
+                      GestureDetector(
+                        onTap: () async {
+                          if (isrecording) {
+                            photo = await _cameraController!
+                                .stopVideoRecording();
+                            _stopRecordingAndUpload(); // Upload video automatically
+                          } else {
+                            await _cameraController!.startVideoRecording();
+                          }
+                          setState(() {
+                            isrecording = !isrecording;
+                          });
+                        },
+                        child: Icon(
+                          isrecording
+                              ? Icons.radio_button_on
+                              : Icons.panorama_fish_eye,
+                          color: isrecording ? Colors.red : Colors.white,
+                          size: 70,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () async {
+                          setState(() {
+                            flip = !flip;
+                            flash = false;
+                          });
+                          int n = flip ? 0 : 1;
+                          _cameraController = CameraController(
+                              camera![n], ResolutionPreset.high);
+                          cameraValue = _cameraController!.initialize();
+                        },
+                        icon: Icon(
+                          Icons.camera_roll,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  show
+                      ? Container(
+                          width: 300,
+                          child: Slider(
+                              max: 10,
+                              min: 1,
+                              value: zoom.toDouble(),
+                              onChanged: (v) async {
+                                await _cameraController!
+                                    .setZoomLevel(zoom.toDouble());
+                                setState(() {
+                                  zoom = v.toInt();
+                                  log(v.roundToDouble().toString());
+                                });
+                              }),
+                        )
+                      : Text(
+                          'Tap: Start Recording & Tap: Stop Recording',
+                          style: TextStyle(color: Colors.grey),
+                        )
+                ],
               ),
             ),
           ],
