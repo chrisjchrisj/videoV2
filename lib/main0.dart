7122bd5ea70bd06777c7f4ed1e4a7bd3b03b718a
chris <chrisjames123@hotmail.com>
2024-02-14 14:35:07 -0800

    added revised

---
+++ lib/main0.dart
@@ -0,0 +1,52 @@
+import 'package:camera/camera.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:video/camera_page.dart';
+import 'package:video/login_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  camera = await availableCameras();
+  await Firebase.initializeApp();
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginScreen(), // Navigate to LoginScreen
+    );
+  }
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Text(
+          'Video Recording/Streaming App',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => CameraScreen()));
+        },
+        label: const Text('Start'),
+      ),
+    );
+  }
+}
