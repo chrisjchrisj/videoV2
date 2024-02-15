070feb98ed371ec7e3d76839ab3f89a732bcbc24
chris <chrisjames123@hotmail.com>
2024-02-15 09:57:27 -0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:camera/camera.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:video/camera_page.dart';
-//import 'package:video/login_screen.dart';
 
 List<CameraDescription>? camera;
 
@@ -21,36 +20,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //home: LoginScreen(), // Navigate to LoginScreen
-    );
-  }
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: const Center(
-        child: Text(
-          'Video Recording/Streaming App',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => CameraScreen()),
-          );
-        },
-        label: const Text('Start'),
-      ),
+      home: CameraScreen(), // Display CameraScreen directly
     );
   }
 }
