1efd5d7e8ffb6f65dc7cfead25bb5f1958e1eaa2
chris <chrisjames123@hotmail.com>
2024-02-16 15:13:44 -0800

    mod

--- lib/camera_page.dart
+++ lib/camera_page.dart
@@ -1,8 +1,5 @@
-//021624-238
-// ignore_for_file: non_constant_identifier_names, prefer_const_constructors, use_build_context_synchronously
+//021624-313
 import 'dart:developer';
-import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
@@ -23,14 +20,13 @@ class _CameraScreenState extends State<CameraScreen> {
 
   @override
   void initState() {
+    _cameraController = CameraController(camera!.first, ResolutionPreset.high);
+    _cameraValue = _initializeCamera();
     super.initState();
-    _initializeCamera();
   }
 
-  void _initializeCamera() async {
-    final cameras = await availableCameras();
-    _cameraController = CameraController(cameras[0], ResolutionPreset.high);
-    _cameraValue = _cameraController.initialize();
+  Future<void> _initializeCamera() async {
+    await _cameraController.initialize();
   }
 
   @override
@@ -42,7 +38,6 @@ class _CameraScreenState extends State<CameraScreen> {
   bool flip = true;
   bool flash = false;
   XFile? photo;
-
   bool isRecording = false;
   int zoom = 1;
   bool show = false;
@@ -62,67 +57,19 @@ class _CameraScreenState extends State<CameraScreen> {
                   return Center(
                     child: CircularProgressIndicator(),
                   );
-                } else if (snapshot.hasError) {
-                  return Center(
-                    child: Text('Error: ${snapshot.error}'),
-                  );
-                } else {
-                  return GestureDetector(
-                    onTap: () async {
-                      await _cameraController.setFocusMode(FocusMode.auto);
-                    },
-                    onLongPress: () async {
-                      await _cameraController.setFocusMode(FocusMode.locked);
-                    },
-                    child: CameraPreview(_cameraController),
-                  );
                 }
+                return GestureDetector(
+                  onTap: () async {
+                    await _cameraController.setFocusMode(FocusMode.auto);
+                  },
+                  onLongPress: () async {
+                    await _cameraController.setFocusMode(FocusMode.locked);
+                  },
+                  child: CameraPreview(_cameraController),
+                );
               },
             ),
-            Align(
-              alignment: Alignment.topCenter,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: Icon(
-                      Icons.close,
-                      color: Colors.white,
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      if (!flip) {
-                        flash = false;
-                      } else {
-                        setState(() {
-                          flash = !flash;
-                        });
-                      }
-                      _cameraController.setFlashMode(
-                          flash ? FlashMode.torch : FlashMode.off);
-                    },
-                    icon: Icon(
-                      flash ? Icons.flash_on : Icons.flash_off,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Positioned(
-              bottom: 10,
-              right: 0,
-              left: 0,
-              child: Column(
-                children: [
-                  // Widgets for zoom and other controls
-                ],
-              ),
-            ),
+            // Other widgets...
           ],
         ),
       ),
